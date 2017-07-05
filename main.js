041f34d55afdb9f0c034efa01a99aaff59b9f5cd
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-07-05 08:31:49 -0400

    update

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-// console.log("This is javascript");
-// let filterInput = document.getElementById('filterInput');
+ console.log("This is javascript");
+let filterInput = document.getElementById('filterInput');
 
-// console.log(filterInput);
+//console.log(filterInput);
 
-// filterInput.addEventListener('keyup', filterNames);
+filterInput.addEventListener('keyup', function filterNames(){
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    console.log(filterValue);
+    let ul = document.getElementById('names');
 
-// function filterNames() {
-//     console.log(1);
-//     let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    let li = ul.querySelectorAll('li.collection-item');
 
-//     let ul = document.getElementById('names');
+    //console.log(li.length);
 
-//     let li = ul.querySelectorAll('li-collection-item');
+    for(let i = 0; i < li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
 
-//     console.log(li.length);
-
-//     for(let i = 0; i < li.length; i++){
-//         let a = li[i].getElementsByTagName('a')[0];
-
-//         if(a.innerHTML.toLocaleUpperCase().indexOf(filterValue)>-1){
-//             li[i].style.display = '';
-//         } else {
-//             li[i].style.display = 'none';
-//         }
-//     }
-// }
+        if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+});
