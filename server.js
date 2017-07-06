ed71def4de3ebd01f0e5acab75fdaa9548ddbbfb
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-07-06 02:09:06 -0400

    add module and change sth

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 // var main = require("./main.js");
+var data_service = ("./data_service.js");
 var path = require("path");
 var app = express();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -9,8 +12,27 @@ app.listen(HTTP_PORT, function onHttpStart(){
   console.log("Express http server listening on :"+ HTTP_PORT);
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: 'layout',
+    helpers: {
+        equal: (lvalue, rvalue, options) => {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }
+    }
+}));
+app.set("view engine", ".hbs");
+
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.render("index");
+    // res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.use(express.static('js'));
